c66a295c30229eb953082042c2efe8d3014046a5
Noel Colon <ncolon@us.ibm.com>
2022-03-18 15:50:23 -0400

    deploy c1 c2 c3 c4
    Signed-off-by: Noel Colon <ncolon@us.ibm.com>

---
+++ 0-bootstrap/others/3-multi-cluster/rhacm-cluster-template/new-cluster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+C_DIR=$(dirname ${BASH_SOURCE})
+NEW_CLUSTER=$1
+if [ -z "${NEW_CLUSTER}" ]; then
+  echo "Usage: $0 <new-cluster-name>"
+  exit 1
+fi
+cp -Rp $C_DIR ../$NEW_CLUSTER
+pushd $C_DIR/../$NEW_CLUSTER
+
+find . -name '*.yaml' -print0 |
+  while IFS= read -r -d '' File; do
+    if grep -q "kind: Application\|kind: AppProject" "$File"; then
+      #echo "$File"
+      sed -i'.bak' -e "s#\${RHACM_CLUSTER_TEMPLATE}#${NEW_CLUSTER}#" $File
+      rm "${File}.bak"
+    fi
+  done
+echo "git commit and push changes now"
